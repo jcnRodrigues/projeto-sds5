5519568981ec3d201da7931d8f6cd7a762695f4a
Joao C N Rodrigues <claudio.analista.de.sistemas@gmail.com>
2021-11-02 13:48:26 -0300

    Project Created

---
+++ backend/src/main/java/com/dvsuperior/dsvends/DsvendsApplication.java
@@ -0,0 +1,13 @@
+package com.dvsuperior.dsvends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dvsuperior/dsvends/DsvendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvsuperior.dsvends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
